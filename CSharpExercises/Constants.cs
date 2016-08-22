9dcf15fadf11a6bbe17d11c2567fdd736251c309
Cameron Wilby <cameron@origincodeacademy.com>
2016-08-21 21:25:28 -0700

    Initial Commit

---
+++ CSharpExercises/Constants.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpExercises
+{
+    class Constants
+    {
+        public const string Quote = "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.";
+        public const string QuoteReversed = ".ti gubed ot hguone trams ton ,noitinifed yb ,era uoy ,elbissop sa ylrevelc sa edoc eht etirw uoy fi ,eroferehT .ecalp tsrif eht ni edoc eht gnitirw sa drah sa eciwt si gniggubeD";
+        public const string TimesTableFor9 = "9 * 1 = 9\r\n9 * 2 = 18\r\n9 * 3 = 27\r\n9 * 4 = 36\r\n9 * 5 = 45\r\n9 * 6 = 54\r\n9 * 7 = 63\r\n9 * 8 = 72\r\n9 * 9 = 81\r\n9 * 10 = 90";
+        public const string TriangleFor8and5 = "88888\r\n8888\r\n888\r\n88\r\n8";
+        public static DateTime[] Next7Days = new DateTime[] { new DateTime(2016, 8, 22), new DateTime(2016, 8, 23), new DateTime(2016, 8, 24), new DateTime(2016, 8, 25), new DateTime(2016, 8, 26), new DateTime(2016, 8, 27), new DateTime(2016, 8, 28) };
+        public const string DuckGooseResult = "1\r\n2\r\nDuck\r\n4\r\nGoose\r\nDuck\r\n7\r\n8\r\nDuck\r\nGoose\r\n11\r\nDuck\r\n13\r\n14\r\nDuckGoose\r\n16\r\n17\r\nDuck\r\n19\r\nGoose\r\nDuck\r\n22\r\n23\r\nDuck\r\nGoose\r\n26\r\nDuck\r\n28\r\n29\r\nDuckGoose\r\n31\r\n32\r\nDuck\r\n34\r\nGoose\r\nDuck\r\n37\r\n38\r\nDuck\r\nGoose\r\n41\r\nDuck\r\n43\r\n44\r\nDuckGoose\r\n46\r\n47\r\nDuck\r\n49\r\nGoose\r\nDuck\r\n52\r\n53\r\nDuck\r\nGoose\r\n56\r\nDuck\r\n58\r\n59\r\nDuckGoose\r\n61\r\n62\r\nDuck\r\n64\r\nGoose\r\nDuck\r\n67\r\n68\r\nDuck\r\nGoose\r\n71\r\nDuck\r\n73\r\n74\r\nDuckGoose\r\n76\r\n77\r\nDuck\r\n79\r\nGoose\r\nDuck\r\n82\r\n83\r\nDuck\r\nGoose\r\n86\r\nDuck\r\n88\r\n89\r\nDuckGoose\r\n91\r\n92\r\nDuck\r\n94\r\nGoose\r\nDuck\r\n97\r\n98\r\nDuck\r\nGoose";
+    }
+}
---
+++ CSharpExercises/Exercises.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpExercises
+{
+    public static class Exercises
+    {
+        // Example Exercise:
+        // Create a method called HelloWorld that returns the following string - "Hello World!"
+        public static string HelloWorld()
+        {
+            return "Hello World!";
+        }
+
+        /* Alright - your turn now! */
+
+        // 1. Create a method called SayHello that accepts a string representing a name and returns a welcome message (E.g. "Hello John!")
+        // 2. Create a method called Sum that accepts two integers and returns their sum.
+        // 3. Create a method called Divide that accepts two decimals and returns the result of dividing the two numbers as a decimal.
+        // 4. Create a method called CanOpenCheckingAccount that accepts an integer representing a customers age, returning true if the age is greater than or equal to 18, or false if the argument is less than 18.
+        // 5. Create a method called GetFirstName that accepts a string representing a full name (e.g. "John Smith"), and returns only the first name.
+        // 6. Create a method called ReverseStringHard that accepts a string and returns the string in reverse. (No built in functions allowed)
+        // 7. Create a method called ReverseStringEasy that accepts a string and returns the string in reverse. (Using only built in functions)
+        // 8. Create a method called PrintTimesTable that accepts an integer and returns the times table as a string for that number up to the 10th multiplication.
+        /* e.g. 10 should return
+         * 10 * 1 = 10
+         * 10 * 2 = 20
+         * 10 * 3 = 30
+         * 10 * 4 = 40
+         * 10 * 5 = 50
+         * 10 * 6 = 60
+         * 10 * 7 = 70
+         * 10 * 8 = 80
+         * 10 * 9 = 90
+         * 10 * 10 = 10 */
+
+        // 9. Create a method called ConvertKelvinToFahrenheit that accepts a double representing a temperature in kelvin and returns a double containing the temperature in Fahrenheit.
+        // 10. Create a method called GetAverageHard that accepts an array of integers and returns the average value as a double. (No built in functions allowed)
+        // 11. Create a method called GetAverageEasy that accepts an array of integers and returns the average value as a double. (Using only built in functions)
+        // 12. Create a method called DrawTriangle that accepts two integers - number and width and returns a string containing a drawn triangle using the number parameter.
+        /* e.g. Number: 8, Width: 8 should return
+         * 88888888
+         * 8888888
+         * 888888
+         * 88888
+         * 8888
+         * 888
+         * 88
+         * 8 */
+
+        // 13. Create a method called GetMilesPerHour that accepts a double representing distance and three integers representing hours, minutes and seconds. The method should return the speed in MPH rounded to the nearest whole number as a string. (e.g. "55MPH")
+
+
+        // 14. Create a method called IsVowel that accepts a char parameter and returns true if the parameter is a vowel or false if the parameter is a consonant.
+        // 15. Create a method called IsConsonant that accepts a char parameter and returns true if the parameter is a consonant or false if the parameter is a vowel.
+        // 16. The Collatz conjecture, named after Lothar Collatz of Germany, proposed the following conjecture in 1937. 
+        // Beginning with an integer n > 1, repeat the following until n == 1. If n is even, halve it. If n is odd, triple it and add 1. Following these steps, the function will always arrive at the number 1.
+        // Create a method called CollatzConjecture that accepts an integer and returns the number of steps required to get to n == 1 as an integer.
+
+        // 17. Create a method called GetNext7Days that accepts a DateTime object and returns an array of DateTime objects containing the next 7 days (including the given day).
+        // 18. Create a method called IsInLeapYear that accepts a DateTime object and returns true if the date falls within a leap year and false if not. (No built in functions allowed)
+        // 19. Create a method called MortgageCalculator that accepts 2 decimals representing loan balance and interest rate, an integer representing loan term in years, and an integer representing the payment period.
+        /* Payment periods: 1 - Monthly, 2 - Bi-Monthly (Every 2 months) */
+
+        // 20. Create a method called DuckGoose that accepts an integer. Iterate from 1 to this integer, building a string along the way.
+        // If the current number in the iteration:
+        //   Is divisible by 3, append "Duck" + Environment.NewLine; to the string.
+        //   Is divisible by 5, append "Goose" + Environment.NewLine; to the string.
+        //   Is divisible by both 3 and 5, append "DuckGoose" + Environment.NewLine; to the string.
+        //   Is none of the above, append the number as a string + Environment.NewLine; to the string.
+        /* Example - if the input to this method is 20, the following string should be returned
+         * 1
+         * 2
+         * Duck
+         * 4
+         * Goose
+         * Duck
+         * 7
+         * 8
+         * Duck
+         * Goose
+         * 11
+         * Duck
+         * 13
+         * 14
+         * DuckGoose
+         * 16
+         * 17
+         * Duck
+         * 19
+         * Goose
+         */
+
+        // If you've finished all these challenges, sign up for CodeWars.com and try to complete a few C# challenges there!
+    }
+}
